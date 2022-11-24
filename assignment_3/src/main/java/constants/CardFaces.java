d8b6bba1b4e9d54071194add4ec293bb66e7353f
Amrhe-Minott <60018839+Amrhe-Minott@users.noreply.github.com>
2022-11-23 19:15:14 -0500

    CODE handle scoring for a KING card

---
+++ assignment_3/src/main/java/constants/CardFaces.java
@@ -0,0 +1,10 @@
+package constants;
+
+public class CardFaces {
+
+	public static final String KING = "K";
+
+	public static final String QUEEN = "Q";
+
+	public static final String JACK = "J";
+}
--- assignment_3/src/main/java/crazyeight/gamelogic/GameLogic.java
+++ assignment_3/src/main/java/crazyeight/gamelogic/GameLogic.java
@@ -2,14 +2,24 @@ package crazyeight.gamelogic;
 
 import java.util.ArrayList;
 
+import constants.CardFaces;
+
 public class GameLogic {
+	final int SCORE_TEN = 10;
 
 	public int scoreRound(ArrayList<String> playerHand) {
+		int finalScore = 0;
 		if (playerHand.size() == 0) {
-			return 0;
+			return finalScore;
+		}
+		
+		for (String card : playerHand) {
+			if (card.contains(CardFaces.KING)) {
+				finalScore += SCORE_TEN;
+			}
 		}
 
-		return Integer.MAX_VALUE;
+		return finalScore;
 	}
 
 }
