245856dff3b9757fd5df411c81ca450b9166841b
Amrhe-Minott <60018839+Amrhe-Minott@users.noreply.github.com>
2022-11-23 19:08:35 -0500

    U-TEST handle case for scoring a card that is KING

--- assignment_3/src/test/java/crazyeight/gamelogic/GameLogicTest.java
+++ assignment_3/src/test/java/crazyeight/gamelogic/GameLogicTest.java
@@ -17,4 +17,11 @@ public class GameLogicTest {
 
 		assertEquals(gameLogic.scoreRound(playerHand), 0);
 	}
+
+	@Test
+	public void givenKing_whenScoreRound_assertScoreOfTen() {
+		ArrayList<String> playerHand = new ArrayList<>(Arrays.asList("KH"));
+
+		assertEquals(gameLogic.scoreRound(playerHand), 10);
+	}
 }
