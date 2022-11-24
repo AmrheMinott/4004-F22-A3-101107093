9afe830d7cac000ac742d3af6237dbfb3f1b8b84
Amrhe-Minott <60018839+Amrhe-Minott@users.noreply.github.com>
2022-11-23 19:01:55 -0500

    U-TEST test t handle when player has empty hand

---
+++ assignment_3/src/test/java/crazyeight/gamelogic/GameLogicTest.java
@@ -0,0 +1,20 @@
+package crazyeight.gamelogic;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+public class GameLogicTest {
+
+	private GameLogic gameLogic = new GameLogic();
+
+	@Test
+	public void givenEmptyHand_whenScoreRound_assertScoreOfZero() {
+		ArrayList<String> playerHand = new ArrayList<>(Arrays.asList());
+
+		assertEquals(gameLogic.scoreRound(playerHand), 0);
+	}
+}
