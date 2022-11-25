3227568b1861d439fb28ade68062ba363dd60bde
Amrhe-Minott <60018839+Amrhe-Minott@users.noreply.github.com>
2022-11-25 15:22:36 -0500

    CODE get card from deck when chosen

--- assignment_3/src/main/java/crazyeight/gamelogic/GameLogic.java
+++ assignment_3/src/main/java/crazyeight/gamelogic/GameLogic.java
@@ -1,6 +1,8 @@
 package crazyeight.gamelogic;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 import constants.CardFaces;
 import crazyeight.websocket.spring.model.Player;
@@ -9,6 +11,28 @@ public class GameLogic {
 	final int SCORE_ONE = 1;
 	final int SCORE_TEN = 10;
 	final int SCORE_FIFTY = 50;
+	private String currentPlayerName = "";
+
+	private ArrayList<String> deck = new ArrayList<>(Arrays.asList("2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H",
+			"10H", "JH", "QH", "KH", "AH", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS",
+			"AS", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD", "AD", "2C", "3C", "4C", "5C",
+			"6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AC"));
+
+	public GameLogic() {
+		Collections.shuffle(deck);
+	}
+
+	public String drawCard(String playerName) {
+		if (!playerName.equals(currentPlayerName)) {
+			return takeCard();
+		}
+
+		return null;
+	}
+
+	public String takeCard() {
+		return this.deck.remove(0);
+	}
 
 	public int scoreRound(ArrayList<String> playerHand) {
 		int finalScore = 0;
@@ -61,5 +85,13 @@ public class GameLogic {
 			updatedPlayers.add(p);
 		}
 		return updatedPlayers;
+	}
+
+	public ArrayList<String> getDeck() {
+		return deck;
+	}
+
+	public void setDeck(ArrayList<String> deck) {
+		this.deck = deck;
 	}
 }
