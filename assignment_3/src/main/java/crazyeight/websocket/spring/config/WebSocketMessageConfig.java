e700473e5300a7d6b5bf4205055b0215875a25d3
Amrhe-Minott <60018839+Amrhe-Minott@users.noreply.github.com>
2022-11-29 19:02:45 -0500

    REFACTOR added user handle to separate message for read abaility of code

--- assignment_3/src/main/java/crazyeight/websocket/spring/config/WebSocketMessageConfig.java
+++ assignment_3/src/main/java/crazyeight/websocket/spring/config/WebSocketMessageConfig.java
@@ -12,7 +12,7 @@ public class WebSocketMessageConfig implements WebSocketMessageBrokerConfigurer 
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
-		config.enableSimpleBroker("/topic");
+		config.enableSimpleBroker("/user", "/topic");
 		config.setApplicationDestinationPrefixes("/app");
 	}
 
